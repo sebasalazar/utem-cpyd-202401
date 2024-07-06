f008e5b60cc242d9131f4b0b3cde6f1af3c66f9b
Sebastián Salazar Molina <sebasalazar@gmail.com>
2024-07-06 10:07:20 -0400

    Modelo inicial del proyecto

---
+++ app/ReST/src/test/java/cl/utem/ws/WsApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.utem.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
